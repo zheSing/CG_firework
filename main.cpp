61f75878b48f5a2c7d7fb1828f4d1a5216969646
zheSing <48282392+zheSing@users.noreply.github.com>
2020-12-14 15:13:42 +0800

    change gl version

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ int main()
     // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // 创建窗口
